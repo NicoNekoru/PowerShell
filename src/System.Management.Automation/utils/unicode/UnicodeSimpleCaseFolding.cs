beecb8822bc61fc49dea81b4ed068a15327b9ee0
iSazonov <darpa@yandex.ru>
2018-11-01 16:35:34 +0500

    Got fast SpanFold and improve perf tests

--- src/System.Management.Automation/utils/unicode/UnicodeSimpleCaseFolding.cs
+++ src/System.Management.Automation/utils/unicode/UnicodeSimpleCaseFolding.cs
@@ -2,7 +2,9 @@
 // Licensed under the MIT License.
 
 using System.Collections.Generic;
+using System.Globalization;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
@@ -18,18 +20,38 @@ namespace System.Management.Automation.Unicode
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static char Fold(char ch)
         {
-            return (char)s_simpleCaseFoldingTableBMPane1[ch];
+            return s_simpleCaseFoldingTableBMPane1[ch];
 
         }
 
         /// <summary>
         /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        //[MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static string Fold(this string source)
         {
             return string.Create(source.Length, source , (chars, sourceString) =>
             {
                 SpanFold(chars, sourceString);
+            });
+        }
+
+        /// <summary>
+        /// </summary>
+        //[MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string FoldBase(this string source)
+        {
+            /*
+            var tmp = new char[source.Length];
+            for (var i = 0; i < source.Length; i++)
+            {
+                tmp[i] = Fold(source[i]);
+            }
+
+            return tmp.ToString();
+            */
+            return string.Create(source.Length, source , (chars, sourceString) =>
+            {
+                SpanFoldBase(chars, sourceString);
             });
         }
 
@@ -59,18 +81,208 @@ namespace System.Management.Automation.Unicode
         internal const char LOW_SURROGATE_END = '\udfff';
         internal const int  HIGH_SURROGATE_RANGE = 0x3FF;
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static void SpanFold(Span<char> result, ReadOnlySpan<char> source)
+        //[MethodImpl(MethodImplOptions.AggressiveInlining)]
+/*        private static void SpanFoldBase(Span<char> result, ReadOnlySpan<char> source)
         {
-            for (int i = 0; i < source.Length; i++)
+            var length = source.Length;
+
+            for (int i = 0; i < length; i++)
             {
                 var ch = source[i];
 
-                if (ch < HIGH_SURROGATE_START)
+                if (IsAscii(ch))
                 {
-                    result[i] = Fold(ch);
+                    if((uint)(ch - 'A') <= (uint)('Z' - 'A'))
+                    {
+                        result[i] = (char)(ch | 0x20);
+                    }
+                    else
+                    {
+                         result[i] = ch;
+                    }
+
+                    continue;
+                }
+
+                if (ch < HIGH_SURROGATE_START || ch > LOW_SURROGATE_END)
+                {
+                    result[i] = s_simpleCaseFoldingTableBMPane1[ch];
+                }
+                else
+                {
+                    if ((i + 1) < length)
+                    {
+                        var ch2 = source[i + 1];
+                        if ((ch2 >= LOW_SURROGATE_START) && (ch2 <= LOW_SURROGATE_END))
+                        {
+                            // The index is Utf32 - 0x10000 (UNICODE_PLANE01_START)
+                            var index = ((ch - HIGH_SURROGATE_START) * 0x400) + (ch2 - LOW_SURROGATE_START);
+                            // The utf32 is Utf32 - 0x10000 (UNICODE_PLANE01_START)
+                            var utf32 = s_simpleCaseFoldingTableBMPane2[index];
+                            result[i] = (char)((utf32 / 0x400) + (int)HIGH_SURROGATE_START);
+                            i++;
+                            result[i] = (char)((utf32 % 0x400) + (int)LOW_SURROGATE_START);
+                        }
+                        else
+                        {
+                            // Broken unicode - throw?
+                            result[i] = ch;
+                        }
+                    }
+                    else
+                    {
+                        // Broken unicode - throw?
+                        result[i] = ch;
+                    }
                 }
             }
+        }
+*/
+
+        private static void SpanFoldBase(Span<char> result, ReadOnlySpan<char> source)
+        {
+            ref char res = ref MemoryMarshal.GetReference(result);
+            ref char src = ref MemoryMarshal.GetReference(source);
+            var simpleCaseFoldingTableBMPane1 = s_simpleCaseFoldingTableBMPane1.AsSpan();
+            var simpleCaseFoldingTableBMPane2 = s_simpleCaseFoldingTableBMPane2.AsSpan();
+
+            var length = source.Length;
+            int i = 0;
+            var ch = src;
+
+            for (; i < length; i++)
+            {
+                //var ch = source[i];
+                ch = Unsafe.Add(ref src, i);
+
+                if (IsAscii(ch))
+                {
+                    if((uint)(ch - 'A') <= (uint)('Z' - 'A'))
+                    {
+                        //result[i] = (char)(ch | 0x20);
+                        Unsafe.Add(ref res, i) = (char)(ch | 0x20);
+                    }
+                    else
+                    {
+                         //result[i] = ch;
+                         Unsafe.Add(ref res, i) = ch;
+                    }
+
+                    continue;
+                }
+
+                if (ch < HIGH_SURROGATE_START || ch > LOW_SURROGATE_END)
+                {
+                    //result[i] = (char)s_simpleCaseFoldingTableBMPane1[ch];
+                    //Unsafe.Add(ref res, i) = s_simpleCaseFoldingTableBMPane1[ch];
+                    Unsafe.Add(ref res, i) = simpleCaseFoldingTableBMPane1[ch];
+                }
+                else
+                {
+                    if ((i + 1) < length)
+                    {
+                        var ch2 = Unsafe.Add(ref src, 1);
+                        if ((ch2 >= LOW_SURROGATE_START) && (ch2 <= LOW_SURROGATE_END))
+                        {
+                            // The index is Utf32 - 0x10000 (UNICODE_PLANE01_START)
+                            var index = ((ch - HIGH_SURROGATE_START) * 0x400) + (ch2 - LOW_SURROGATE_START);
+                            // The utf32 is Utf32 - 0x10000 (UNICODE_PLANE01_START)
+                            var utf32 = simpleCaseFoldingTableBMPane2[index];
+                            Unsafe.Add(ref res, i) = (char)((utf32 / 0x400) + (int)HIGH_SURROGATE_START);
+                            i++;
+                            Unsafe.Add(ref res, i) = (char)((utf32 % 0x400) + (int)LOW_SURROGATE_START);
+                        }
+                        else
+                        {
+                            // Broken unicode - throw?
+                            Unsafe.Add(ref res, i) = ch;
+                        }
+                    }
+                    else
+                    {
+                        // Broken unicode - throw?
+                        Unsafe.Add(ref res, i) = ch;
+                    }
+                }
+            }
+        }
+
+        //[MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void SpanFold(Span<char> result, ReadOnlySpan<char> source)
+        {
+            ref char res = ref MemoryMarshal.GetReference(result);
+            ref char src = ref MemoryMarshal.GetReference(source);
+            //var simpleCaseFoldingTableBMPane1 = s_simpleCaseFoldingTableBMPane1.AsSpan();
+            //var simpleCaseFoldingTableBMPane2 = s_simpleCaseFoldingTableBMPane2.AsSpan();
+            ref char simpleCaseFoldingTableBMPane1 = ref MemoryMarshal.GetReference(s_simpleCaseFoldingTableBMPane1.AsSpan());
+            ref char simpleCaseFoldingTableBMPane2 = ref MemoryMarshal.GetReference(s_simpleCaseFoldingTableBMPane2.AsSpan());
+
+            var length = source.Length;
+            int i = 0;
+            var ch = src;
+
+            for (; i < length; i++)
+            {
+                //var ch = source[i];
+                ch = Unsafe.Add(ref src, i);
+
+                if (IsAscii(ch))
+                {
+                    if((uint)(ch - 'A') <= (uint)('Z' - 'A'))
+                    {
+                        //result[i] = (char)(ch | 0x20);
+                        Unsafe.Add(ref res, i) = (char)(ch | 0x20);
+                    }
+                    else
+                    {
+                         //result[i] = ch;
+                         Unsafe.Add(ref res, i) = ch;
+                    }
+
+                    continue;
+                }
+
+                if (ch < HIGH_SURROGATE_START || ch > LOW_SURROGATE_END)
+                {
+                    //result[i] = (char)s_simpleCaseFoldingTableBMPane1[ch];
+                    //Unsafe.Add(ref res, i) = s_simpleCaseFoldingTableBMPane1[ch];
+                    //Unsafe.Add(ref res, i) = simpleCaseFoldingTableBMPane1[ch];
+                    Unsafe.Add(ref res, i) = Unsafe.Add(ref simpleCaseFoldingTableBMPane1, ch);
+                }
+                else
+                {
+                    if ((i + 1) < length)
+                    {
+                        var ch2 = Unsafe.Add(ref src, 1);
+                        if ((ch2 >= LOW_SURROGATE_START) && (ch2 <= LOW_SURROGATE_END))
+                        {
+                            // The index is Utf32 - 0x10000 (UNICODE_PLANE01_START)
+                            var index = ((ch - HIGH_SURROGATE_START) * 0x400) + (ch2 - LOW_SURROGATE_START);
+                            // The utf32 is Utf32 - 0x10000 (UNICODE_PLANE01_START)
+                            var utf32 = Unsafe.Add(ref simpleCaseFoldingTableBMPane2, index);
+                            Unsafe.Add(ref res, i) = (char)((utf32 / 0x400) + (int)HIGH_SURROGATE_START);
+                            i++;
+                            Unsafe.Add(ref res, i) = (char)((utf32 % 0x400) + (int)LOW_SURROGATE_START);
+                        }
+                        else
+                        {
+                            // Broken unicode - throw?
+                            Unsafe.Add(ref res, i) = ch;
+                        }
+                    }
+                    else
+                    {
+                        // Broken unicode - throw?
+                        Unsafe.Add(ref res, i) = ch;
+                    }
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static bool IsAscii(char c)
+        {
+            return c < 0x80;
         }
 
         /// <summary>
