ded7dfbf71ce04aa060317703edb7338801aa83d
iSazonov <darpa@yandex.ru>
2018-11-09 10:48:04 +0500

    Remove duplicate tests from CharTests

--- test/xUnit/System.Management.Automation/Unicode/CharTests.cs
+++ test/xUnit/System.Management.Automation/Unicode/CharTests.cs
@@ -74,7 +74,6 @@ namespace System.Management.Automation.Unicode.Tests
         {
             yield return new object[] { "\uD800\uDC00", 0, 0x10000 };
             yield return new object[] { "\uDBBF\uDFFF", 0, 0xFFFFF };
-            yield return new object[] { "\uDBBF\uDFFF", 0, 0xFFFFF };
             yield return new object[] { "\uDBFF\uDC00", 0, 0x10FC00 };
             yield return new object[] { "\uDBFF\uDFFF", 0, 0x10FFFF };
             yield return new object[] { "\u0000\u0001", 0, 0 };
@@ -121,7 +120,6 @@ namespace System.Management.Automation.Unicode.Tests
 
         public static IEnumerable<object[]> ConvertToUtf32_Char_Char_TestData()
         {
-            yield return new object[] { '\uD800', '\uDC00', 0x10000 };
             yield return new object[] { '\uD800', '\uDC00', 0x10000 };
             yield return new object[] { '\uD800', '\uDFFF', 0x103FF };
             yield return new object[] { '\uDBBF', '\uDFFF', 0xFFFFF };
